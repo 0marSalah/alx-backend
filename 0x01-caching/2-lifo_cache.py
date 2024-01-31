69bb4e1619d9ca7395efcd52eb6614c92a89b8f2
0marSalah <omar.salah1597@gmail.com>
2024-01-31 17:49:32 +0200

    Implement Last-In-First-Out (LIFO) caching strategy in LIFOCache class

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+""" Create a class FIFOCache that inherits from BaseCaching
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ Implements a Last-In-First-Out (LIFO) caching strategy.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.last_key = None
+
+    def put(self, key, item):
+        """ Put an item into the cache.
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                del self.cache_data[key]
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                print("DISCARD:", self.last_key)
+                del self.cache_data[self.last_key]
+            self.cache_data[key] = item
+            self.last_key = key
+
+    def get(self, key):
+        """ Get an item from the cache by key.
+        """
+        return self.cache_data.get(key, None)
