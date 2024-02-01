019e62867fc2b8dec645663700de3009ba200f73
0marSalah <omar.salah1597@gmail.com>
2024-02-01 13:35:23 +0200

    Add MRUCache class to implement a most-recently-used caching algorithm

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,1 +1,51 @@
 #!/usr/bin/env python3
+""" MRUCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache class:
+    - inherits from BaseCaching and is a caching system
+    - uses a most-recently-used (MRU) algorithm for cache replacement
+    """
+
+    def __init__(self):
+        """ Initialize
+        """
+        super().__init__()
+        self.key_order = []  # Stores the keys in order of most recent usage
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        - If key or item is None, this method should not do anything.
+        - If the number of items in self.cache_data is higher than BaseCaching.MAX_ITEMS,
+          discard the most recently used item.
+        """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+        if key in self.key_order:
+            self.key_order.remove(key)  # Remove key if it already exists
+        self.key_order.append(key)  # Add key as the most recently used
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            mru_key = self.key_order[-2]  # Second last item is the MRU
+            self.cache_data.pop(mru_key)
+            self.key_order.remove(mru_key)
+            print(f"DISCARD: {mru_key}")
+
+    def get(self, key):
+        """ Get an item by key
+        - If key is None or if the key doesn’t exist in self.cache_data, return None.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        # Update key order since it's been accessed
+        self.key_order.remove(key)
+        self.key_order.append(key)
+        return self.cache_data[key]
