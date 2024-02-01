20eaed2714ad5cb8f76ab34e091a1038a509b082
0marSalah <omar.salah1597@gmail.com>
2024-02-01 13:26:21 +0200

    Implement LRUCache class for Least-Recently-Used caching strategy

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,1 +1,33 @@
 #!/usr/bin/env python3
+""" Create a class LRUCache that inherits from BaseCaching
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """ Implements a Least-Recently-Used (LRU) caching strategy.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Put an item into the cache.
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                least_key = next(iter(self.cache_data))
+                del self.cache_data[least_key]
+                print("DISCARD:", least_key)
+
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key)
+
+    def get(self, key):
+        """ Get an item from the cache by key.
+        """
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        return self.cache_data.get(key, None)
